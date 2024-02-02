44549a59ab88b8786a7903d4ecd154690b7e58a5
Casey R <crowlandsdev@gmail.com>
2024-02-02 08:15:39 -0500

    pushing up

---
+++ src/api/infoblock-home/routes/infoblock-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * infoblock-home router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::infoblock-home.infoblock-home');
---
+++ src/api/infoblock-home/services/infoblock-home.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * infoblock-home service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::infoblock-home.infoblock-home');
